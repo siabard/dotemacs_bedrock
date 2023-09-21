7d76ca2965e0c7550806ebbf1e7a37734a986695
Yeonho Jang <siabard@gmail.com>
2023-09-21 16:35:31 +0900

    LSP 수정

--- modules/prelude-lsp.el
+++ modules/prelude-lsp.el
@@ -10,18 +10,16 @@
 
 
 (with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"
-					"-j=8"
-					"--std=c++17"
-					"--log=error"
-					"--malloc-trim"
-					"--background-index"
-					"--clang-tidy"
-					"--cross-file-rename"
-					"--completion-style=detailed"
-					"--pch-storage=memory"
-					"--header-insertion=never"
-					"--header-insertion-decorators=0")))
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) . ( "clangd"
+							      "-j=8"
+							      "--log=error"
+							      "--background-index"
+							      "--clang-tidy"
+							      "--cross-file-rename"
+							      "--completion-style=detailed"
+							      "--pch-storage=memory"
+							      "--header-insertion=never"
+							      "--header-insertion-decorators=0"))))
 
 (provide 'prelude-lsp)
 ;;; prelude-lsp.el ends here
