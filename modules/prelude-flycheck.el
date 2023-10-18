c2b2fbf7d08cb03e3cdf5ede7df83772fcacf61a
Yeonho Jang <siabard@gmail.com>
2023-10-18 09:37:00 +0900

    flymake 비활성 처리 / c++ include path 추가

--- modules/prelude-flycheck.el
+++ modules/prelude-flycheck.el
@@ -4,9 +4,15 @@
   (global-flycheck-mode t)
   :config
   (setq flycheck-cppcheck-standards "c++17")
-  :hook ((find-file-hook . (lambda () (flymake-mode -1)))
-	 ((c-or-c++-mode-hook c++-mode-hook) . (lambda () (setq flycheck-clang-language-standard "c++17")))
-	 ((c-or-c++-mode-hook c++-mode-hook) . (lambda () (setq flycheck-gcc-language-standard "c++17"))))
+  :hook ((find-file . (lambda () (flymake-mode -1)))
+	 (eglot--managed-mode . (lambda () (flymake-mode -1)))
+	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-clang-language-standard "c++17")))
+	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-gcc-language-standard "c++17")))
+	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-clang-include-path
+								(list (expand-file-name "/usr/local/include")
+								      (expand-file-name "/opt/homebrew/include")
+								      (expand-file-name "/usr/include")
+								      (expand-file-name "/usr/local/opt/llvm/include/c++/v1"))))))
   )
 
 (flymake-mode -1)
