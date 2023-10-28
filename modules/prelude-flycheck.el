01db71629ecfd96c009da94d9fe0522a7218d703
yhjang <siabard@gmail.com>
2023-10-28 17:15:20 +0900

    저장, 모드 변경시에만 flycheck 하도록 수정

--- modules/prelude-flycheck.el
+++ modules/prelude-flycheck.el
@@ -4,14 +4,15 @@
   (global-flycheck-mode t)
   :config
   (setq flycheck-cppcheck-standards "c++17")
+  (setq flycheck-check-syntax-automatically '(mode-enabled save))
   :hook ((find-file . (lambda () (flymake-mode -1)))
 	 (eglot--managed-mode . (lambda () (flymake-mode -1)))
 	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-clang-language-standard "c++17")))
 	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-gcc-language-standard "c++17")))
 	 ((c-or-c++-mode c++-mode c++-ts-mode) . (lambda () (setq flycheck-clang-include-path
 								(list (expand-file-name "/usr/local/include")
-								      (expand-file-name "/opt/homebrew/include")
-								      (expand-file-name "/usr/include")
+								      (expand-file-name "/opt/local/include")
+								      ;; (expand-file-name "/usr/include")
 								      (expand-file-name "/usr/local/opt/llvm/include/c++/v1"))))))
   )
 
